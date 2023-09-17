6fdc41b26fb0dcc3dc017d041ea3eab4d0984428
Rupam Sutar <rupamsutar281998@gmail.com>
2023-09-17 13:56:38 +0530

    Pages Created !

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { About, Cart, Checkout, Error, HomeLayout, Landing, Login, Orders, Products, Register, SingleProduct } from "./pages"
+
 const App = () => {
   return (
     <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      Hello world!
+    </h1>
   )
 }
 
-export default App
+export default App``
