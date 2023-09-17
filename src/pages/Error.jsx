07f61f7c8199a0f4ab3967f7622fa6862c0872b3
Rupam Sutar <rupamsutar281998@gmail.com>
2023-09-17 14:33:34 +0530

    Route Structure Complete

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useRouteError } from 'react-router-dom'
 
 const Error = () => {
+  const error = useRouteError();
+  console.log(error);
   return (
-    <div>Error</div>
+    <main className='grid min-h-[100vh] place-items-center px-8'>
+      <h4 className='text-center font-bold text-4xl'> There was an error</h4>
+    </main>
   )
 }
 
