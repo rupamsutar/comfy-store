534996ed5684ccdb880ba4f66b093d6bed41a133
Rupam Sutar <rupamsutar281998@gmail.com>
2023-09-17 20:26:53 +0530

    navbar structure complete

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Header } from "../components";
+import { Header, Navbar } from "../components";
 
 const HomeLayout = () => {
   return (
     <>
       <Header />
+      <Navbar />
       <section className="align-element py-20">
         <Outlet />
       </section>
