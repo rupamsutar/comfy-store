69019bd4da22fb81d891b7bad7a5847fb7d23e7b
Rupam Sutar <rupamsutar281998@gmail.com>
2023-09-17 19:36:03 +0530

    Header Component

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Header } from "../components";
 
 const HomeLayout = () => {
   return (
     <>
-      <nav>
-        <span className="text-4xl text-primary">Comfy</span>
-      </nav>
+      <Header />
       <section className="align-element py-20">
         <Outlet />
       </section>
