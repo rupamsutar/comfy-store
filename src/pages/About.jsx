05d253d8304632c9ccd10ed881d13722c9d79bfc
Rupam Sutar <rupamsutar281998@gmail.com>
2023-09-18 21:42:14 +0530

    Setting about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+      <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+        We Love
+      </h1>
+      <div className="stats bg-primary shadow">
+        <div className="stat">
+          <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+            comfy
+          </div>
+        </div>
+      </div>
+    </div>
+    <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto text-justify">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid eum aperiam dolorum. Rerum maiores nisi itaque autem ullam minus magni ipsum animi consequuntur mollitia, dolorem aut fugit nobis, voluptas repellendus.
+    </p>
+    </>
   )
 }
 
