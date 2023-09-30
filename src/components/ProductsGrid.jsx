e52b43d3b0523954fb80e6af9ff9d7e760289b22
Rupam Sutar <rupamsutar281998@gmail.com>
2023-09-30 19:10:45 +0530

    Format Price

--- src/components/ProductsGrid.jsx
+++ src/components/ProductsGrid.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useLoaderData } from "react-router-dom";
+import { formatPrice } from "../utils";
 
 const ProductsGrid = () => {
   const { products } = useLoaderData();
@@ -8,7 +9,7 @@ const ProductsGrid = () => {
     <div className="pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
       {products.map((product) => {
         const { title, price, image } = product.attributes;
-
+        const rupeesAmount = formatPrice(price);
         return (
           <Link
             key={product.id}
@@ -20,7 +21,7 @@ const ProductsGrid = () => {
             </figure>
             <div className="card-body items-center text-center">
               <h2 className="card-title capitalize tracking-wider">{title}</h2>
-              <span className="text-secondary">{price}</span>
+              <span className="text-secondary">&#8377; {rupeesAmount}</span>
             </div>
           </Link>
         );
--- src/utils/index.js
+++ src/utils/index.js
@@ -4,4 +4,13 @@ const productionUrl = 'https://strapi-store-server.onrender.com/api';
 
 export const customFetch = axios.create({
     baseURL: productionUrl
-})
+});
+
+export const formatPrice = (price) => {
+    const rupeesAmount = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'INR'
+    }).format(((price/100)*82).toFixed(2));
+
+    return rupeesAmount;
+}
