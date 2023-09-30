0b3357eb252c9da0a55f247d9ce3a8e47008a3e8
Rupam Sutar <rupamsutar281998@gmail.com>
2023-09-30 19:46:45 +0530

    Render Single Product

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { customFetch } from '../utils';
+import { customFetch, formatPrice } from '../utils';
+import { Link } from 'react-router-dom';
 
 export const loader = async({params}) => {
   const response = await customFetch(`/products/${params.id}`);
@@ -10,9 +11,33 @@ export const loader = async({params}) => {
 
 const SingleProduct = () => {
   const {product} = useLoaderData();
-  console.log(product);
+  const {image, title, price, description, colors, company} = product.attributes;
+  const rupeesAmount = formatPrice(price);
   return (
-    <div>SingleProduct</div>
+    <section>
+      <div className="text-md breadcrumbs">
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/products'>products</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="mt-d grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
+        <img src={image} alt={title} className='w-96 h-96 object-cover rounded-lg lg:w-full' />
+      
+      <div className="">
+        <h1 className='capitalize text-3xl font-bold'>{title}</h1>
+        <h4 className='text-xl text-neutral-content font-bold mt-2'>
+          {company}
+        </h4>
+        <p className="mt-3 text-xl">{rupeesAmount}</p>
+        <p className='mt-6 leading-8'>{description}</p>
+      </div>
+      </div>
+    </section>
   )
 }
 
