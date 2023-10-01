002c466dafe0a818a1262b46b15c09bb2fa85a5f
Rupam Sutar <rupamsutar281998@gmail.com>
2023-10-01 10:37:54 +0530

    Filters Select Inputs complete

--- src/components/index.js
+++ src/components/index.js
@@ -12,5 +12,6 @@ export {default as FeaturedProducts} from "./FeaturedProducts";
 export {default as Filters} from "./Filters";
 export {default as ProductsContainer} from "./ProductsContainer";
 export {default as PaginationContainer} from "./PaginationContainer";
+export {default as FormSelect} from "./FormSelect";
 
 
