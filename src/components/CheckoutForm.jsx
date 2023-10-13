cc2a5a5cc9f0edba4c6e0d3ff56b5f55b62c129b
Rupam Sutar <rupamsutar281998@gmail.com>
2023-10-13 22:20:22 +0530

    Checkout Form - Complete

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,22 +1,62 @@
-import React from 'react';
-import { Form } from 'react-router-dom';
-import FormInput from './FormInput';
-import SubmitBtn from './SubmitBtn';
+import React from "react";
+import { Form, redirect } from "react-router-dom";
+import FormInput from "./FormInput";
+import SubmitBtn from "./SubmitBtn";
+import { customFetch, formatPrice } from "../utils";
+import { clearCart } from "../features/cart/cartSlice";
 
-export const action  = (store) => async() => {
-    console.log(store);
+export const action =
+  (store) =>
+  async ({ request }) => {
+    const formData = await request.formData();
+    const { name, address } = Object.fromEntries(formData);
+    const user = store.getState().userState.user;
+    const { cartItems, orderTotal, numItemsInCart } =
+      store.getState().cartState;
+
+    const info = {
+      name,
+      address,
+      chargeTotal: orderTotal,
+      orderTotal: formatPrice(orderTotal),
+      cartItems,
+      numItemsInCart,
+    };
+
+    try {
+      const response = await customFetch.post(
+        "/orders",
+        { data: info },
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+      store.dispatch(clearCart());
+      toast.success("order placed successfully!");
+      return redirect("/orders");
+    } catch (error) {
+      const errorMessage =
+        error?.response?.data?.error?.message ||
+        "There was an error placing your order!";
+      toast.error(errorMessage);
+    }
+
     return null;
-}
+  };
 
 const CheckoutForm = () => {
- return <Form method='POST' className='flex flex-col gap-y-8'>
-    <h4 className="font-medium text-xl capitalize">shipping information</h4>
-    <FormInput label='first name' name='name' type='text' />
-    <FormInput label='address' name='address' type='text' />
-    <div className="mt-4">
-        <SubmitBtn text='Place your order' />
-    </div>
- </Form>
-}
+  return (
+    <Form method="POST" className="flex flex-col gap-y-8">
+      <h4 className="font-medium text-xl capitalize">shipping information</h4>
+      <FormInput label="first name" name="name" type="text" />
+      <FormInput label="address" name="address" type="text" />
+      <div className="mt-4">
+        <SubmitBtn text="Place your order" />
+      </div>
+    </Form>
+  );
+};
 
-export default CheckoutForm
+export default CheckoutForm;
