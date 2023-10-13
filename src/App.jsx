21750336ce48d18c3c564aa71fdcb95c502db06c
Rupam Sutar <rupamsutar281998@gmail.com>
2023-10-13 23:36:15 +0530

    Orders Page - Setup

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import {loader as landingLoader} from "./pages/Landing";
 import {loader as singleProductLoader} from "./pages/SingleProduct";
 import {loader as productsLoader} from "./pages/Products";
 import {loader as checkoutLoader} from "./pages/Checkout";
+import {loader as ordersLoader} from "./pages/Orders";
 // Actions
 import {action as registerAction} from './pages/Register';
 import {action as loginAction} from "./pages/Login";
@@ -54,7 +55,8 @@ const router = createBrowserRouter([
       },
       {
         path: 'orders',
-        element: <Orders />
+        element: <Orders />,
+        loader: ordersLoader(store) 
       }
     ]
   },
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { redirect } from "react-router-dom";
+import { toast } from "react-toastify";
+import { customFetch } from "../utils";
+
+export const loader =
+  (store) =>
+  async ({ request }) => {
+    const user = store.getState().userState.user;
+    if (!user) {
+      toast.warn("You must be logged in to view the orders");
+      return redirect("/login");
+    }
+    const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries(),
+    ]);
+    try {
+      const response = await customFetch.get("/orders", {
+        params,
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      return { orders: response.data.data, meta: response.deta.meta };
+    } catch (error) {
+      const errorMessage =
+        error?.response?.data?.error?.message ||
+        "There was an error placing your order!";
+      toast.error(errorMessage);
+      if (error.response.status === 401) return redirect("/login");
+      return null;
+    }
+  };
 
 const Orders = () => {
-  return (
-    <div>Orders</div>
-  )
-}
+  return <div>Orders</div>;
+};
 
-export default Orders
+export default Orders;
